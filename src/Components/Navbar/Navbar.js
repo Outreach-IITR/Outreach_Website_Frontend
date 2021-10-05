7e7ea427fdb95fae9b2c48d8c1f0656957f1b975
Sainath <sainatharao@gmail.com>
2021-10-05 15:52:45 +0530

    Logo Fix

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import "./Navbar.css";
-import logo from './Assets/logo.png';
+import logo from './Assets/logo.svg';
 
 const Navbar = () => {
     const [showList, setShowList] = useState(false);

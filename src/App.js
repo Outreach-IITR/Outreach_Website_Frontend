86b996a53750226b325473efddb471128dd35517
rohitkb0112 <76957463+rohitkb0112@users.noreply.github.com>
2021-10-05 17:14:04 +0530

    updates and blog section done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import Footer from './Components/Footer/Footer';
 import Homepage from './Components/Homepage/Homepage'
 import Navbar from './Components/Navbar/Navbar'
+import Updates from './Components/Homepage/Updates';
+import Blog from './Components/Homepage/Blog';
+
+
 
 function App() {
   return (
     <div>
       <Navbar />
       <Homepage />
+      <Updates />
+      <Blog />
       <Footer/>
     </div>
   );
---
+++ src/Components/Homepage/Updates.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import Updateimage from './Assets/updates_home.png'
+import './Updates.css'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
+
+class Updates extends React.Component {
+    render() {
+        return (
+            <><div id="updatessection">
+            <div id="updates_first_section" data-aos="fade-right" data-aos-offset="400" data-aos-easing="ease-in-sine">
+                <h1>Updates</h1>
+                <div id="update_content">
+                    <div id="update_content_data">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum duis enim parturient et, a cursus elit enim egestas.
+                         <br/> <a>Know More</a>
+                    </p>
+                    
+                    </div>
+                    <div id="update_content_data">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum duis enim parturient et, a cursus elit enim egestas.
+                         <br/> <a>Know More</a>
+                    </p>
+                    
+                    </div>
+                    <div id="update_content_data">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum duis enim parturient et, a cursus elit enim egestas.
+                         <br/> <a>Know More</a>
+                    </p>
+                    
+                    </div>
+                    <div id="update_content_data">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum duis enim parturient et, a cursus elit enim egestas.
+                         <br/> <a>Know More</a>
+                    </p>
+                    
+                    </div>
+                    
+                    
+
+                </div>
+            </div>
+            <div id="updates_image" data-aos="fade-left" data-aos-anchor="#example-anchor" data-aos-offset="800" data-aos-easing="ease-in-sine">
+            <img alt="img" src={Updateimage} />
+            </div>
+
+        </div></>
+        )
+    }}
+    export default Updates;

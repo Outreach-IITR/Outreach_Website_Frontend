6a49b4dbfc2875c3d403fbbb91ec803d2172a4e4
Sainath <sainatharao@gmail.com>
2021-10-06 02:45:21 +0530

    Footer_inital_commit

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,88 +1,132 @@
 import React, { Component } from 'react'
 import "./Footer.css";
-import facebook from './Assets/facebook.png';
-import linkedin from './Assets/linkedin.png';
+import facebook from './Assets/facebook.svg';
+import linkedin from './Assets/linkedin.svg';
 import outreach_logo from './Assets/outreach_logo.svg';
-import twitter from './Assets/twitter.png';
-import youtube from './Assets/youtube.png';
+import twitter from './Assets/twitter.svg';
+import youtube from './Assets/youtube.svg';
 
-export class Footer extends Component {
-    render() {
+const Footer = () => {
+ 
         return (
-            <div className="footer">
-                <div className="footer-content">
-                    <div className="footer-subcontent">
-                    <h3 className="f-heading h-link">Quick&#160;links</h3>
-                        <ul style={{'list-style' : 'none'}}>
-                            <li>Lorem Ipsum</li>
-                            <li>Lorem Ipsum</li>
-                            <li>Lorem Ipsum</li>
-                            <li>Lorem Ipsum</li>
-                            <li>Lorem Ipsum</li>
-
-                        </ul>
-                    </div>
-                    <div className="footer-subcontent">
-                        <h3 className="f-heading h-acads">Academics</h3>
-                        <ul style={{'list-style' : 'none'}}>
-                            <li>Lorem Ipsum</li>
-                            <li>Lorem Ipsum</li>
-                            <li>Lorem Ipsum</li>
-
-                        </ul>
-                    </div>
-                </div>
-                <div className="footer-content" >
-                    <div className="footer-subcontent">
-                        <h3 className="f-heading h-life">Life at IITR</h3>
-                        <ul style={{'list-style' : 'none'}}>
-                            <li>Lorem Ipsum</li>
-                            <li>Lorem Ipsum</li>
-                            <li>Lorem Ipsum</li>
-                            <li>Lorem Ipsum</li>
-
-                        </ul>
-                    </div>
-                    <div className="footer-subcontent">
-                        <h3 className="f-heading h-about">About Us</h3>
-                        <ul style={{'list-style' : 'none'}}>
-                            <li>About Outreach</li>
-                            <li>About IIT Roorkee</li>
-                            <li>Outreach Team</li>
-
-                        </ul>
-                    </div>
-                </div>
+            <div className="main-footer">
                 
-                <div className="footer-rightcontent">
-                    <div className="footer-info">
-                        <img id="footer_logo" src={outreach_logo} alt="outreach logo"/>
-                    </div>
-                    <div className="footer-info">
-                        <p className="footer-text">
-                            <br/>
-                            +91 0123456789<br/><br/>
-                            outreach@iitr.ac.in<br/><br/>
-                            SAC Building, IIT Roorkee,<br/>
-                            Roorkee, Uttarakhand, India - 247667
-                            <br/>
-                        </p>
-                    </div>
-                    
-                    <div className="f-social">
-                        <a href="/" className="logo-links"><img src={facebook} width="20px" alt="facebook"/></a>
-                        <a href="/" className="logo-links"><img src={twitter} width="20px" height="" alt="twitter"/></a>
-                        <a href="/" className="logo-links"><img src={linkedin} width="20px" alt="linkedin"/></a>
-                        <a href="/" className="logo-links"><img src={youtube} width="20px" height="" alt="youtube"/></a>
 
+                <div className="flex-item-left">
+                    <div className="quick-links-div">
+                        <h3 id="footer-quick-links">Quick&#160;Links</h3>
+                            <ul className="footer-column">
+                                <li className="footer-column-item">
+                                    <a href="/" className="footer-item">Lorem Ipsum</a>
+                                </li>
+                                <li className="footer-column-item">
+                                    <a href="/" className="footer-item">Lorem ipsum 22</a>
+                                </li>
+                                <li className="footer-column-item">
+                                    <a href="/" className="footer-item">Testing Lorem</a>
+                                </li>
+                                <li className="footer-column-item">
+                                    <a href="/" className="footer-item">Lorem Ipsum</a>
+                                </li>
+                                <li className="footer-column-item">
+                                    <a href="/" className="footer-item">Lorem Ipsum</a>
+                                </li>
+                            </ul>    
                     </div>
-                    <div className="footer-bottom">
-                        &copy; 2021 Copyright, All rights reserved 
+
+                </div>
+
+                <div className="flex-item-left">
+                    <div className="academics-div">
+                        <h3 id="footer-academics">Academics</h3>
+                            <ul className="footer-column">
+                                <li className="footer-column-item">
+                                    <a href="/" className="footer-item">Departments</a>
+                                </li>
+                                <li className="footer-column-item">
+                                    <a href="/" className="footer-item">Lorem ipsum 22</a>
+                                </li>
+                            </ul>    
+                    </div>
+                </div>
+
+                <div className="flex-item-left">
+                    <div className="life-div">
+                        <h3 id="footer-life">Life&#160;at&#160;IIT&#160;R</h3>
+                            <ul className="footer-column">
+                                <li className="footer-column-item">
+                                    <a href="/" className="footer-item">Lorem Ipsum</a>
+                                </li>
+                                <li className="footer-column-item">
+                                    <a href="/" className="footer-item">Lorem ipsum 22</a>
+                                </li>
+                                <li className="footer-column-item">
+                                    <a href="/" className="footer-item">Testing Lorem</a>
+                                </li>
+                                <li className="footer-column-item">
+                                    <a href="/" className="footer-item">Lorem Ipsum</a>
+                                </li>
+                                <li className="footer-column-item">
+                                    <a href="/" className="footer-item">Lorem Ipsum</a>
+                                </li>
+                            </ul>    
+                    </div>
+
+                </div>
+
+                <div className="flex-item-left">
+                    <div className="about-us-div">
+                        <h3 id="footer-about-us">About&#160;Us</h3>
+                            <ul className="footer-column">
+                                <li className="footer-column-item">
+                                    <a href="/" className="footer-item">About&#160;Outreach</a>
+                                </li>
+                                <li className="footer-column-item">
+                                    <a href="/" className="footer-item">About&#160;IIT&#160;Roorkee</a>
+                                </li>
+                                <li className="footer-column-item">
+                                    <a href="/" className="footer-item">Outreach&#160;Team</a>
+                                </li>
+                            </ul>    
+                    </div>
+
+                </div>
+
+
+                
+                <div className="flex-item-right">
+                    <div className="right-section">
+                        <img alt="img" id="outreach_logo" src={outreach_logo} />
+                        <p className="footer-right-text">
+                            <br/>
+                            +91&#160;0123456789<br/><br/>
+                            outrach@iitr.ac.in<br/><br/>
+                            SAC Building, IIT Roorkee,<br/>
+                            Roorkee, Uttrakhand, India - 247667
+                        </p>
+                        <div className="icons">
+                            <a href="/" target="_blank" rel="noreferrer">
+                                <img alt="img" src={facebook} />
+                            </a>
+                            <a href="/" target="_blank" rel="noreferrer">
+                                <img alt="img" src={twitter} />
+                            </a>
+                            <a href="/" target="_blank" rel="noreferrer">
+                                <img alt="img" src={linkedin} />
+                            </a>
+                            <a href="/" target="_blank" rel="noreferrer">
+                                <img alt="img" src={youtube} />
+                            </a>
+                        </div>  
+                        <p className="footer-right-text" id="copy-right">
+                            <br/>
+                            2021 Copyright, All rights reserved
+                        </p>                   
                     </div>
                 </div>
             </div>
         )
-    }
 }
+
 
 export default Footer;

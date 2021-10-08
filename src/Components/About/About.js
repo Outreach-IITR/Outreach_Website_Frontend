6d073d2dfd1c68b8baf28e7322f926ea8eb68eb1
Sainath <sainatharao@gmail.com>
2021-10-08 19:34:24 +0530

    imported navbarfix directory

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 
 import OurTeam from "./OurTeam";
+import NavbarFix from "../NavbarFix/NavbarFix"
 
 class App extends Component {
   render() {
     return (
       <div>
+        <NavbarFix />
         <OurTeam />
       </div>
     );
---
+++ src/Components/NavbarFix/NavbarFix.js
@@ -0,0 +1,67 @@
+import React, {useState, useEffect} from 'react';
+import "./NavbarFix.css";
+import logo from './Assets/logo.svg';
+
+
+const NavbarFix = () => {
+    
+    const [showList, setShowList] = useState(false);
+    const [navbar, setHeader] = useState("main-nav")
+
+    const listenScrollEvent = (event) => {
+        if (window.scrollY < 80) {
+          return setHeader("main-nav")
+        } else if (window.scrollY > 7) {
+          return setHeader("main-nav-fix")
+        } 
+      }
+      
+      useEffect(() => {
+        window.addEventListener('scroll', listenScrollEvent);
+      
+        return () =>
+          window.removeEventListener('scroll', listenScrollEvent);
+      }, []);
+
+        return (
+            <nav className={navbar}>
+                <div className="logo">
+                    <img alt="img" src={logo} />
+                </div>
+
+                <div className= {showList ? "mobile-menu-link" : "menu-link"}>
+                    <ul className="menu-link-ul">
+                        <li className="menu-link-item">
+                            <a href="/" className="menu-item">About</a>
+                        </li>
+                        <li className="menu-link-item">
+                            <a href="/" className="menu-item">Academics</a>
+                        </li>
+                        <li className="menu-link-item">
+                            <a href="/" className="menu-item">Achievements</a>
+                        </li>
+                        <li className="menu-link-item">
+                            <a href="/" className="menu-item">Life&nbsp;at&nbsp;IITR</a>
+                        </li>
+                        <li className="menu-link-item">
+                            <a href="/" className="menu-item">Placements</a>
+                        </li>
+                    </ul>      
+                </div>
+
+                <div className="Discussion-forum">
+                <a href="/" target="_blank" rel="noreferrer">
+                    <button id="join_button" className={showList ? "Discussion-forum-button" : "Discussion-forum-button-list Discussion-forum-button"}>
+                        Join&nbsp;the&nbsp;Discussion&nbsp;forum
+                    </button>
+                </a>
+                </div>
+                <div className="List-menu" onClick={() => setShowList(!showList)}>
+                    <i className={showList ? "chevron up icon List-menu-icon" : "chevron down icon List-menu-icon"} ></i>
+                </div>
+            </nav>
+        )
+}
+
+
+export default NavbarFix;

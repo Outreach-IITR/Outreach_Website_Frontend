3b50344366118ce7f025c25b8897ffa9f6932173
Rahul Modi <rahul_km@ee.iitr.ac.in>
2021-10-08 10:51:17 +0530

    Our Team Section added

---
+++ src/Components/About/TeamMembers.js
@@ -0,0 +1,371 @@
+import image1 from "./Assets/test_girl.png";
+import image2 from "./Assets/test_boy.png";
+
+const Secretaries = [
+  {
+    name: "SName1",
+    image: image1,
+  },
+  {
+    name: "SName2",
+    image: image2,
+  },
+  {
+    name: "SName3",
+    image: image1,
+  },
+  {
+    name: "SName4",
+    image: image2,
+  },
+  {
+    name: "SName5",
+    image: image1,
+  },
+  {
+    name: "SName1",
+    image: image1,
+  },
+  {
+    name: "SName2",
+    image: image2,
+  },
+  {
+    name: "SName3",
+    image: image1,
+  },
+  {
+    name: "SName4",
+    image: image2,
+  },
+  {
+    name: "SName5",
+    image: image1,
+  },
+  {
+    name: "SName1",
+    image: image1,
+  },
+  {
+    name: "SName2",
+    image: image2,
+  },
+];
+
+const Design = [
+  {
+    name: "DeName1",
+    image: image2,
+  },
+  {
+    name: "DeName2",
+    image: image1,
+  },
+  {
+    name: "DeName3",
+    image: image2,
+  },
+  {
+    name: "DeName4",
+    image: image1,
+  },
+  {
+    name: "DeName5",
+    image: image2,
+  },
+  {
+    name: "DeName1",
+    image: image2,
+  },
+  {
+    name: "DeName2",
+    image: image1,
+  },
+  {
+    name: "DeName3",
+    image: image2,
+  },
+  {
+    name: "DeName4",
+    image: image1,
+  },
+  {
+    name: "DeName5",
+    image: image2,
+  },
+  {
+    name: "DeName1",
+    image: image1,
+  },
+  {
+    name: "DeName2",
+    image: image2,
+  },
+];
+
+const Development = [
+  {
+    name: "DevName1",
+    image: image1,
+  },
+  {
+    name: "DevName2",
+    image: image2,
+  },
+  {
+    name: "DevName3",
+    image: image2,
+  },
+  {
+    name: "DevName4",
+    image: image2,
+  },
+  {
+    name: "DevName5",
+    image: image2,
+  },
+  {
+    name: "DevName1",
+    image: image1,
+  },
+  {
+    name: "DevName2",
+    image: image2,
+  },
+  {
+    name: "DevName3",
+    image: image2,
+  },
+  {
+    name: "DevName4",
+    image: image2,
+  },
+  {
+    name: "DevName5",
+    image: image2,
+  },
+  {
+    name: "DevName1",
+    image: image1,
+  },
+  {
+    name: "DevName2",
+    image: image2,
+  },
+];
+
+const Multimedia = [
+  {
+    name: "MName1",
+    image: image1,
+  },
+  {
+    name: "MName2",
+    image: image2,
+  },
+  {
+    name: "MName3",
+    image: image2,
+  },
+  {
+    name: "MName4",
+    image: image2,
+  },
+  {
+    name: "MName5",
+    image: image2,
+  },
+  {
+    name: "MName1",
+    image: image1,
+  },
+  {
+    name: "MName2",
+    image: image2,
+  },
+  {
+    name: "MName3",
+    image: image2,
+  },
+  {
+    name: "MName4",
+    image: image2,
+  },
+  {
+    name: "MName5",
+    image: image2,
+  },
+  {
+    name: "MName1",
+    image: image1,
+  },
+  {
+    name: "MName2",
+    image: image2,
+  },
+];
+
+const Operations = [
+  {
+    name: "OName1",
+    image: image1,
+  },
+  {
+    name: "OName2",
+    image: image2,
+  },
+  {
+    name: "OName3",
+    image: image2,
+  },
+  {
+    name: "OName4",
+    image: image2,
+  },
+  {
+    name: "OName5",
+    image: image2,
+  },
+  {
+    name: "OName1",
+    image: image1,
+  },
+  {
+    name: "OName2",
+    image: image2,
+  },
+  {
+    name: "OName3",
+    image: image2,
+  },
+  {
+    name: "OName4",
+    image: image2,
+  },
+  {
+    name: "OName5",
+    image: image2,
+  },
+  {
+    name: "OName1",
+    image: image1,
+  },
+  {
+    name: "OName2",
+    image: image2,
+  },
+];
+
+const Editorial = [
+  {
+    name: "EDame1",
+    image: image1,
+  },
+  {
+    name: "EDName2",
+    image: image2,
+  },
+  {
+    name: "EDName3",
+    image: image2,
+  },
+  {
+    name: "EDName4",
+    image: image2,
+  },
+  {
+    name: "EDName5",
+    image: image2,
+  },
+  {
+    name: "EDame1",
+    image: image1,
+  },
+  {
+    name: "EDName2",
+    image: image2,
+  },
+  {
+    name: "EDName3",
+    image: image2,
+  },
+  {
+    name: "EDName4",
+    image: image2,
+  },
+  {
+    name: "EDName5",
+    image: image2,
+  },
+  {
+    name: "EDame1",
+    image: image1,
+  },
+  {
+    name: "EDName2",
+    image: image2,
+  },
+];
+
+const SocialMedia = [
+  {
+    name: "MName1",
+    image: image1,
+  },
+  {
+    name: "MName2",
+    image: image2,
+  },
+  {
+    name: "MName3",
+    image: image2,
+  },
+  {
+    name: "MName4",
+    image: image2,
+  },
+  {
+    name: "MName5",
+    image: image2,
+  },
+  {
+    name: "MName1",
+    image: image1,
+  },
+  {
+    name: "MName2",
+    image: image2,
+  },
+  {
+    name: "MName3",
+    image: image2,
+  },
+  {
+    name: "MName4",
+    image: image2,
+  },
+  {
+    name: "MName5",
+    image: image2,
+  },
+  {
+    name: "MName1",
+    image: image1,
+  },
+  {
+    name: "MName2",
+    image: image2,
+  },
+];
+
+const TeamMembers = {
+  Secretaries,
+  Design,
+  Development,
+  Multimedia,
+  Operations,
+  Editorial,
+  "Social Media": SocialMedia,
+};
+
+export default TeamMembers;
---
+++ src/Components/About/TeamTemplate.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import "./TeamTemplate.css";
+
+const TeamTemplate = (props) => {
+  const [background, setBackground] = useState("");
+  const backgroundColor_List = ["#feba10", "#f96508", "#06ca3d", "#026ba1"];
+  const OnHoverHandler = () => {
+    let indx;
+    do {
+      indx = Math.floor(4 * Math.random());
+    } while (background === backgroundColor_List[indx]);
+    setBackground(backgroundColor_List[indx]);
+  };
+  return (
+    <div className="TeamTemplate">
+      {props.TeamList.map((member, indx) => {
+        return (
+          <div
+            key={indx}
+            className="TeamTemplate__ImgCard"
+            onMouseEnter={OnHoverHandler}
+            style={{ "--hover_color": background }}
+          >
+            <img alt="icon" src={member.image} />
+            <h4 className="TeamTemplate__MemberName">{member.name}</h4>
+            <h6 className="TeamTemplate__MemberPost">Secretary</h6>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default TeamTemplate;

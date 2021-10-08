07ef2466448ca1485f14551d1de55af0b2b9a2ef
Rahul Modi <rahul_km@ee.iitr.ac.in>
2021-10-08 18:09:08 +0530

    About Outreach Section added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 
+import Navbar from "../Navbar/Navbar";
+import AboutOutreach from "./AboutOutreach";
 import OurTeam from "./OurTeam";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Navbar />
+        <AboutOutreach />
         <OurTeam />
       </div>
     );

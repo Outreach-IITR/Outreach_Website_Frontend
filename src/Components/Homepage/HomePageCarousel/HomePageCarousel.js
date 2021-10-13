ea7f322f4d2666f80153216605b00c4e5d2ccbfa
Rahul Modi <rahul_km@ee.iitr.ac.in>
2021-10-13 11:54:33 +0530

    Youtube Autoscroll added

--- src/Components/Homepage/HomePageCarousel/HomePageCarousel.js
+++ src/Components/Homepage/HomePageCarousel/HomePageCarousel.js
@@ -1,5 +1,6 @@
 //*************Created By Rahul Modi*************
-import React, { useState, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import YouTube from "react-youtube";
 import "./HomePageCarousel.css";
 
 import { slides } from "./CarouselData";
@@ -8,6 +9,7 @@ import RightArrow from "./Assets/RightArrow.svg";
 
 const HomePageCarousel = () => {
   const [current, setCurrent] = useState(2);
+  const [isVideoPlaying, setIsVideoPlaying] = useState(false);
   const length = slides.length;
 
   //Functions to change slides
@@ -19,17 +21,33 @@ const HomePageCarousel = () => {
     setCurrent(current === 0 ? length - 1 : current - 1);
   };
 
-  // useEffect(() => {
-  //   const timer = setTimeout(() => nextSlide(), 3000);
-  //   return () => {
-  //     clearTimeout(timer);
-  //   };
-  // }, [current, nextSlide]);
+  const onPlayHandler = () => {
+    setIsVideoPlaying(true);
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (!isVideoPlaying) {
+        nextSlide();
+      }
+    }, 3000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [current, nextSlide, isVideoPlaying]);
 
   //to check passed variable is an array and is not empty
   if (!Array.isArray(slides) || slides.length <= 0) {
     return null;
   }
+
+  const opts = {
+    width: "100%",
+    height: "100%",
+    playerVars: {
+      autoplay: 0,
+    },
+  };
 
   return (
     <div
@@ -62,13 +80,12 @@ const HomePageCarousel = () => {
               key={indx}
             >
               {/* Giving each slide a class wrt to its position from current slide */}
-              <iframe
-                className="Img"
-                src={img.video}
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
-              ></iframe>
+              <YouTube
+                containerClassName="Img"
+                videoId={img.video}
+                opts={opts}
+                onPlay={onPlayHandler}
+              />
             </div>
           );
         })}

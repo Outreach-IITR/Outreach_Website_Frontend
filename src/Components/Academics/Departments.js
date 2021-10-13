e1a80d205904390fa872a1f8d8a604f68a14d336
Akshit <Gakshit92@gmail.com>
2021-10-13 23:26:20 +0530

    Department mobile view

--- src/Components/Academics/Departments.js
+++ src/Components/Academics/Departments.js
@@ -7,6 +7,10 @@ import { slides } from "./AcadCarouselData";
 import Crousel from "../Crousel/Crousel";
 import plus from "./Assets/plus.svg";
 import minus from "./Assets/minus.svg";
+import backArrow from "./Assets/BackArrow.svg";
+import forArrow from "./Assets/ForArrow.svg";
+import MyMobile from "../../helperComponents/MyMobile";
+import MyDesktop from "../../helperComponents/MyDesktop";
 
 const Departments = () => {
   const [background, setBackground] = useState("");
@@ -14,6 +18,7 @@ const Departments = () => {
   const [id, setId] = useState("");
   const myRef = useRef();
   const backgroundColor_List = ["#feba10", "#f96508", "#06ca3d", "#026ba1"];
+  const [depPage, setDepPage] = useState(1);
 
   const OnHoverHandler = () => {
     let indx;
@@ -30,27 +35,69 @@ const Departments = () => {
     myRef.current.scrollIntoView();
   };
 
+  const handlePageForward = () => {
+    if (depPage === 4) {
+      return setDepPage(1);
+    }
+    return setDepPage(depPage + 1);
+  };
+
+  const handlePageBackward = () => {
+    if (depPage === 1) {
+      return setDepPage(4);
+    }
+    return setDepPage(depPage - 1);
+  };
+
   return (
     <>
       <h1 id="Departments_heading">Departments</h1>
       <div className="containerD">
-        {list.map((data, idx) => {
-          return (
-            <div
-              className="cardD"
-              key={data.id}
-              onClick={() => handleSelection(idx)}
-              onMouseEnter={OnHoverHandler}
-              style={{
-                backgroundColor: `${id === idx ? clickBackground : ""}`,
-                color: `${id === idx ? "#ffff" : ""}`,
-                "--hover_color": background,
-              }}
-            >
-              <p>{data.dep}</p>
+        <MyMobile>
+          {list.map((data, idx) => {
+            return (
+              <>
+                {idx < 6 * depPage && idx >= 6 * (depPage - 1) ? (
+                  <div
+                    className={`cardD ${
+                      idx === id ? "selectedBackground" : ""
+                    }`}
+                    key={data.id}
+                    onClick={() => handleSelection(idx)}
+                  >
+                    <p>{data.dep}</p>
+                  </div>
+                ) : null}
+              </>
+            );
+          })}
+          <div className="counterDepartment">
+            <img src={backArrow} alt="" onClick={handlePageBackward} />
+            <div>
+              Swipe to navigate <br /> {depPage} of 4
             </div>
-          );
-        })}
+            <img src={forArrow} alt="" onClick={handlePageForward} />
+          </div>
+        </MyMobile>
+        <MyDesktop>
+          {list.map((data, idx) => {
+            return (
+              <div
+                className="cardD"
+                key={data.id}
+                onClick={() => handleSelection(idx)}
+                onMouseEnter={OnHoverHandler}
+                style={{
+                  backgroundColor: `${id === idx ? clickBackground : ""}`,
+                  coloimppr: `${id === idx ? "#ffff" : ""}`,
+                  "--hover_color": background,
+                }}
+              >
+                <p>{data.dep}</p>
+              </div>
+            );
+          })}
+        </MyDesktop>
       </div>
       <div ref={myRef}></div>
       <div

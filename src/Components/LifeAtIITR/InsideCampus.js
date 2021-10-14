c505c6f41520fcf20eadf1e3e5b07656a91994c5
Rahul Modi <rahul_km@ee.iitr.ac.in>
2021-10-14 14:24:25 +0530

    Life At IITR Carousel Added

---
+++ src/Components/LifeAtIITR/InsideCampus.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./InsideCampus.css";
+import Carousel from "../Crousel/Crousel";
+import InsideCampusData from "./InsideCampusData";
+
+const InsideCampus = () => {
+  return (
+    <div className="InsideCampus__Section">
+      <h1 className="InsideCampus__heading">Inside Campus</h1>
+      <Carousel slides={InsideCampusData} />
+      <div className="CampusLife">
+        <h3 className="CampusLife__heading">Campus Life</h3>
+        <p className="CampusLife__para">
+          {
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra, pellentesque morbi egestas lorem cras sed eu. Libero pharetra tellus mi eu aliquam cras.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra, pellentesque morbi egestas lorem cras sed eu.<WHATS ITS LIKE LIVING INSIDE CAMPUS> Libero pharetra tellus mi eu aliquam cras.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra, pellentesque morbi egestas lorem cras sed eu. Libero pharetra tellus mi eu aliquam cras.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra, pellentesque morbi egestas lorem cras sed eu. Libero pharetra tellus mi eu aliquam cras."
+          }
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default InsideCampus;
--- src/Components/LifeAtIITR/Life.js
+++ src/Components/LifeAtIITR/Life.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import NavbarFix from '../NavbarFix/NavbarFix';
-import Groups from './Groups';
+import React from "react";
+import NavbarFix from "../NavbarFix/NavbarFix";
+import InsideCampus from "./InsideCampus";
+import Events from "./Events";
+import Groups from "./Groups";
+import OutsideCampus from "./OutsideCampus";
 
 const Life = () => {
   return (
     <div>
       <NavbarFix />
+      <InsideCampus />
+      <Events />
       <Groups />
+      <OutsideCampus />
     </div>
-  )
-}
+  );
+};
 
-export default Life
+export default Life;

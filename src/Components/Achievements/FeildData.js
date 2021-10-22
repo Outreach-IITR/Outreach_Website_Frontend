bb409fc289b1efdbe067d0e04d675228133f943d
Rahul Modi <rahul_km@ee.iitr.ac.in>
2021-10-22 21:11:07 +0530

    General Achievements Content added

--- src/Components/Achievements/FeildData.js
+++ src/Components/Achievements/FeildData.js
@@ -11,43 +11,37 @@ const Sports = {
   Achievements_List: [
     {
       id: 1,
-      dep: "Sports Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum duis enim parturient et, a cursus elit enim egestas.",
+      dep: "In the 54th inter IIT sports meet women’s Volleyball team won gold medal while Men won bronze medal in Table tennis and squash tournament.",
       link: "/",
       date: "08/07/21",
     },
     {
       id: 2,
-      dep: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum duis enim parturient et, a cursus elit enim egestas.",
+      dep: "In the 53rd sports meet, the women's Basketball and volleyball team won gold medal while Sumit yadav was awarded as the best player of the Hockey.",
       link: "/",
       date: "08/07/21",
     },
     {
       id: 3,
-      dep: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum duis enim parturient et, a cursus elit enim egestas.",
+      dep: "In the 52nd sports meet Aman Pal set a new record in Hammer Throw and Trishna K Chengappa in 100m sprint.",
       link: "/",
       date: "08/07/21",
     },
     {
       id: 4,
-      dep: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum duis enim parturient et, a cursus elit enim egestas.",
+      dep: "In the 50th sports meet men’s basketball, hockey , Football and squash won the gold medal while the silver medal in Tennis. Women won a Bronze medal in Basketball. Also Roorkee came second in the Overall General Championship (Men).",
       link: "/",
       date: "08/07/21",
     },
     {
       id: 5,
-      dep: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum duis enim parturient et, a cursus elit enim egestas.",
+      dep: "In 49th sports meet IIT Roorkee came FIrst in the Overall General Championship (Women).",
       link: "/",
       date: "08/07/21",
     },
     {
       id: 6,
-      dep: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum duis enim parturient et, a cursus elit enim egestas.",
-      link: "/",
-      date: "08/07/21",
-    },
-    {
-      id: 7,
-      dep: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum duis enim parturient et, a cursus elit enim egestas.",
+      dep: "In the 48th sports meet Roorkee won gold in Basketball (women) , Football(men), Squash and swimming , volleyball (women) and silver medal in Badminton(men) , Basketball(men).",
       link: "/",
       date: "08/07/21",
     },
@@ -71,9 +65,6 @@ const Sports = {
     {
       image: BubbleCarousel__2,
     },
-    {
-      image: BubbleCarousel__3,
-    },
   ],
 };
 
@@ -85,25 +76,25 @@ const Technology = {
     {
       id: 1,
       dep: "KNOx achieved 1 Rank in Design Section in Virtuals Round among all IITs.",
-      link: "/",
+      link: "https://new.iitr.ac.in/mechanical/team-knox/",
       date: "08/07/21",
     },
     {
       id: 2,
       dep: "Students from the iGEM IITR have participated for the 1st time and won a gold medal in iGEM (synthetic-biology based competition).",
-      link: "/",
+      link: "https://www.facebook.com/111403760578466/posts/200789174973257/",
       date: "08/07/21",
     },
     {
       id: 3,
       dep: "InfoSec IITR regularly participate in competitive hacking competitions and have been consistently ranked among the top 3 CTF teams in India.",
-      link: "/",
+      link: "http://watchout.iitr.ac.in/Freshman_Guide_To_IITR.pdf",
       date: "08/07/21",
     },
     {
       id: 4,
       dep: "KNOx achievements in BAJA include 2nd place among 326 teams in CAE category (awarded by ANSYS Inc.).",
-      link: "/",
+      link: "https://iitr.ac.in/departments/ME/pages/Events+Student_Sections+KNOx.html",
       date: "08/07/21",
     },
     {
@@ -115,36 +106,50 @@ const Technology = {
     {
       id: 6,
       dep: "33 GSoC selection (Globally highest). ",
-      link: "/",
+      link: "https://opensource.googleblog.com/2021/06/google-summer-of-code-2021-student-stats.html",
       date: "08/07/21",
     },
     {
       id: 7,
       dep: "Highest number of GSoC selection since 2018.",
-      link: "/",
+      link: "https://developers.google.com/open-source/gsoc/resources/stats",
       date: "08/07/21",
     },
     {
       id: 8,
       dep: "IIT Roorkee was one of the premier institutions slated to receive PARAM 10000 supercomputer.",
-      link: "/",
+      link: "https://www.iitr.ac.in/centers/CC/pages/Resources+Param_1000.html",
       date: "08/07/21",
     },
     {
       id: 9,
       dep: "Roorkee Motorsports team achieved 3rd position in Formula Green 2020.",
-      link: "/",
+      link: "https://alumni.iitr.ac.in/iitrnews/formulagreen",
       date: "08/07/21",
     },
     {
       id: 10,
       dep: "ArIES IITR team has qualified for the final among top 10 teams in MathWorks MiniDrone Competition - EMEA (Europe Middle-East Asia) 2021.",
-      link: "/",
+      link: "https://www.facebook.com/503218879766649/posts/4333020526786446/",
       date: "08/07/21",
     },
-    
   ],
   Bubble_List: [
+    {
+      image: BubbleCarousel__1,
+    },
+    {
+      image: BubbleCarousel__2,
+    },
+    {
+      image: BubbleCarousel__3,
+    },
+    {
+      image: BubbleCarousel__4,
+    },
+    {
+      image: BubbleCarousel__5,
+    },
     {
       image: BubbleCarousel__1,
     },

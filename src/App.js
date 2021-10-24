8ccc5c105fe838c4ecbb24aed68b41cdc7f0531d
Rahul Modi <rahul_km@ee.iitr.ac.in>
2021-10-24 20:35:07 +0530

    Loading Screen Added

--- src/App.js
+++ src/App.js
@@ -3,29 +3,48 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Footer from "./Components/Footer/Footer";
 import Homepage from "./Components/Homepage/Homepage";
 import About from "./Components/About/About";
-import Academics from  "./Components/Academics/Academics";
+import Academics from "./Components/Academics/Academics";
 import Achievements from "./Components/Achievements/Achievements";
 import Life from "./Components/LifeAtIITR/Life";
 import Placements from "./Components/Placements/Placements";
+import Loader from "./Components/Loader/Loader";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener("load", () => {
+      setTimeout(() => {
+        this.setState({ isLoading: false });
+      }, 2000);
+    });
+  }
+
   render() {
     return (
-      <div>
-        <div>
-          <BrowserRouter>
-            <Route path="/" exact component={Homepage} />
-            <Route path="/about" exact component={About} />
-            <Route path="/academics" exact component={Academics} />
-            <Route path="/achievements" exact component={Achievements} />
-            <Route path="/lifeatiitr" exact component={Life} />
-            <Route path="/placements" exact component={Placements} />
-          </BrowserRouter>
+      <>
+        {this.state.isLoading && <Loader />}
+        <div style={this.state.isLoading ? { display: "none" } : {}}>
+          <div>
+            <BrowserRouter>
+              <Route path="/" exact component={Homepage} />
+              <Route path="/about" exact component={About} />
+              <Route path="/academics" exact component={Academics} />
+              <Route path="/achievements" exact component={Achievements} />
+              <Route path="/lifeatiitr" exact component={Life} />
+              <Route path="/placements" exact component={Placements} />
+            </BrowserRouter>
+          </div>
+          <div>
+            <Footer />
+          </div>
         </div>
-        <div>
-          <Footer />
-        </div>
-      </div>
+      </>
     );
   }
 }
---
+++ src/Components/Loader/Loader.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Blue_Part from "./Assets/Blue_Part.svg";
+import Orange_Part from "./Assets/Orange_Part.svg";
+import Yellow_Part from "./Assets/Yellow_Part.svg";
+import Green_Part from "./Assets/Green_Part.svg";
+import "./Loader.css";
+
+const Loader = (props) => {
+  return (
+    <div className="Loader">
+      <div id="LoadingText">
+        <h1>Loading</h1>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div id="LogoContainer">
+        <img className="BluePart" src={Blue_Part} alt="logo" />
+        <img className="OrangePart" src={Orange_Part} alt="logo" />
+        <img className="YellowPart" src={Yellow_Part} alt="logo" />
+        <img className="GreenPart" src={Green_Part} alt="logo" />
+      </div>
+    </div>
+  );
+};
+
+export default Loader;

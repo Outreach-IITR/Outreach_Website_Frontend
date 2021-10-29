2c0cd812f8b5459b2d7439d9b750aa2f7b08f067
Rahul Modi <rahul_km@ee.iitr.ac.in>
2021-10-29 21:12:27 +0530

    Scholarships Section and various minor fix

--- src/Components/Academics/Accordion/Accordion.js
+++ src/Components/Academics/Accordion/Accordion.js
@@ -2,127 +2,42 @@ import React from "react";
 
 import "./Accordion.css";
 import ScholarshipImage from "./Assets/scholarshipImage.png";
+import { ScholarshipData } from "./ScholarshipsData";
 
 function Accordion_segment() {
+  const onChangeHandler = (e) => {
+    const isChecked = e.target.checked;
+    const checkbox = document.querySelectorAll(".accordion_input");
+    checkbox.forEach((obj) => (obj.checked = false));
+    e.target.checked = isChecked;
+  };
+
   return (
     <div id="Scholarships" className="accordion_segment_container">
       <h2 className="accordion_segment_header">Scholarships</h2>
       <div className="content_box">
         <div className="accordion">
-          <div>
-            <input type="checkbox" id="section1" class="accordion_input" />
-            <label for="section1" class="accordion__label">
-              <ul className="accordion_segment_section">
-                <li>Meritcum Means Scholarship</li>
-              </ul>
-            </label>
-            <div className="accordion_content">
-              <p>
-                A UG student who is an MCM scholarship beneficiary gets tuition
-                fee reimbursement and additionally ₹1000 per month for 10
-                months. Whereas an MSc. student who is an MCM scholarship
-                beneficiary gets ₹1000 per month for 10 months.
-              </p>
-              <a href="https://scsp.iitr.ac.in/merit-cum-means-2021/">
-                Know more...
-              </a>
-            </div>
-          </div>
-
-          <div>
-            <input type="checkbox" id="section2" class="accordion_input" />
-            <label for="section2" class="accordion__label">
-              <ul className="accordion_segment_section">
-                <li>James Thomson Scholarship</li>
-              </ul>
-            </label>
-            <div className="accordion_content">
-              <p>
-                Students having up to 250 All India Rank in JEE (Advanced) 2021
-                are eligible for James Thomason Scholarship.
-              </p>
-              <a href="https://scsp.iitr.ac.in/james-thomason-scholarship/">
-                Know more...
-              </a>
-            </div>
-          </div>
-
-          <div>
-            <input type="checkbox" id="section3" class="accordion_input" />
-            <label for="section3" class="accordion__label">
-              <ul className="accordion_segment_section">
-                <li>Inspire Scholarship</li>
-              </ul>
-            </label>
-            <div className="accordion_content">
-              <p>
-                This scholarship is offered exclusively to students enrolled in
-                basic science streams a.k.a Integrated MSc courses with a 4
-                digit JEE AIR or less. Eligible candidates may avail 80000/-
-                every year.
-              </p>
-              <a href="https://scsp.iitr.ac.in/inspire-scholarship/">
-                Know more...
-              </a>
-            </div>
-          </div>
-
-          <div>
-            <input type="checkbox" id="section4" class="accordion_input" />
-            <label for="section4" class="accordion__label">
-              <ul className="accordion_segment_section">
-                <li>Encore Scholarship</li>
-              </ul>
-            </label>
-            <div className="accordion_content">
-              <p>
-                Eighty Nine Class of Roorkee Endowment (ENCORE) is an endowment
-                fund established by the IIT Roorkee graduating class of 1989 for
-                the benefit of the Institute, its students, faculty and staff.
-              </p>
-              <a href="https://sites.google.com/site/iitrencore89/">
-                Know more...
-              </a>
-            </div>
-          </div>
-
-          <div>
-            <input type="checkbox" id="section5" class="accordion_input" />
-            <label for="section5" class="accordion__label">
-              <ul className="accordion_segment_section">
-                <li>Heritage Awards</li>
-              </ul>
-            </label>
-            <div className="accordion_content">
-              <p>
-                For young IIT-R graduates, IITRHF provide variety of support in
-                their early years of career development; help develop their
-                skills and self-confidence through interaction with their peers.
-              </p>
-              <a href="https://www.iitr-heritagefund.org/">Know more...</a>
-            </div>
-          </div>
-
-          <div>
-            <input type="checkbox" id="section7" class="accordion_input" />
-            <label for="section7" class="accordion__label">
-              <ul className="accordion_segment_section">
-                <li>Aditya Birla Scholarship</li>
-              </ul>
-            </label>
-            <div className="accordion_content accordion_last_section">
-              <p>
-                Top 15 students on the basis of their JEE advance rank are
-                eligible to apply. Alongwith them 1 student each on the basis of
-                merit cum means, and top ranked students each from Mechanical ,
-                Electrical, Civil and Chemical Engineering are eligible to
-                apply.
-              </p>
-              <a href="https://www.iitr.ac.in/Main/pages/Aditya_Birla_Scholarship.html">
-                Know more...
-              </a>
-            </div>
-          </div>
+          {ScholarshipData.map((obj, indx) => {
+            return (
+              <div key={indx}>
+                <input
+                  type="checkbox"
+                  id={`section${indx}`}
+                  className="accordion_input"
+                  onChange={(e) => onChangeHandler(e)}
+                />
+                <label htmlFor={`section${indx}`} className="accordion__label">
+                  <ul className="accordion_segment_section">
+                    <li>{obj.name}</li>
+                  </ul>
+                </label>
+                <div className="accordion_content">
+                  <p>{obj.desp}</p>
+                  <a href={obj.link}>Know more...</a>
+                </div>
+              </div>
+            );
+          })}
         </div>
 
         <div className="image_box">
---
+++ src/Components/Academics/Accordion/ScholarshipsData.js
@@ -0,0 +1,32 @@
+export const ScholarshipData = [
+  {
+    name: "Meritcum Means Scholarship",
+    desp: " A UG student who is an MCM scholarship beneficiary gets tuition fee reimbursement and additionally ₹1000 per month for 10 months. Whereas an MSc. student who is an MCM scholarship beneficiary gets ₹1000 per month for 10 months.",
+    link: "https://scsp.iitr.ac.in/merit-cum-means-2021/",
+  },
+  {
+    name: "James Thomson Scholarship",
+    desp: "Students having up to 250 All India Rank in JEE (Advanced) 2021 are eligible for James Thomason Scholarship.",
+    link: "https://scsp.iitr.ac.in/james-thomason-scholarship/",
+  },
+  {
+    name: "Inspire Scholarship",
+    desp: "This scholarship is offered exclusively to students enrolled in basic science streams a.k.a Integrated MSc courses with a 4 digit JEE AIR or less. Eligible candidates may avail 80000/- every year.",
+    link: "https://scsp.iitr.ac.in/inspire-scholarship/",
+  },
+  {
+    name: "Encore Scholarship",
+    desp: "Eighty Nine Class of Roorkee Endowment (ENCORE) is an endowment fund established by the IIT Roorkee graduating class of 1989 for the benefit of the Institute, its students, faculty and staff.",
+    link: "https://sites.google.com/site/iitrencore89/",
+  },
+  {
+    name: "Heritage Awards",
+    desp: "For young IIT-R graduates, IITRHF provide variety of support in their early years of career development; help develop their skills and self-confidence through interaction with their peers.",
+    link: "https://www.iitr-heritagefund.org/",
+  },
+  {
+    name: "Aditya Birla Scholarship",
+    desp: "Top 15 students on the basis of their JEE advance rank are eligible to apply. Alongwith them 1 student each on the basis of merit cum means, and top ranked students each from Mechanical, Electrical, Civil and Chemical Engineering are eligible to apply.",
+    link: "https://www.iitr.ac.in/Main/pages/Aditya_Birla_Scholarship.html",
+  },
+];

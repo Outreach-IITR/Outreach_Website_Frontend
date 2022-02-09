2e9ddfb1c6584b6a81ef72a82584ee0cce859f46
ATUL SINGH <atulsingh7151@gmail.com>
2022-02-09 20:24:52 +0530

    Minor_Fixes

--- src/Components/Academics/Assets/AcadCenterData.js
+++ src/Components/Academics/Assets/AcadCenterData.js
@@ -1,11 +1,11 @@
 export const AcadCenter = [
   {
-    name: "Centre of Excellence in Disaster Mitigation and Management (CoEDMM)",
+    name: "Centre of Excellence in Disaster Mitigation and Management",
     description:
       "The Centre of Excellence in Disaster Mitigation and Management (CoEDMM) aims to achieve excellence in critical areas of education and research at par with international standards. Regional issues, primarily related to Earthquakes, Tsunamis, Floods, Cyclones, fires, Avalanche, Land Slides, Cloud Burst, and their early warnings are other focus areas. The center offers a two-year course in disaster mitigation and management specialization. The doctoral program is strongly supported by professors who are stalwarts in their respective fields. The center has faculty with domain expertise in earthquakes, landslides, Cyclones, Floods, Tsunami, Cyber Crime, Avalanche, Fire, Industrial disasters, CBRN (chemical, biological, radiological, and nuclear), and Geomatics Technology. Various research and consultancy projects are being carried out through sponsored funding from Govt. of India and other National and International funding agencies.",
   },
   {
-    name: "Centre for Transportation Systems (CTRANS)",
+    name: "Centre for Transportation Systems",
     description:
       "CTRANS is a Centre of Excellence of IIT Roorkee in Transportation Systems. The Centre aims to evolve a holistic approach for studying transport systems. CTRANS is primarily a multidisciplinary initiative with collective participation of Engineers, Scientists, and Researchers from Science & Technology, Humanities and Social Sciences, Architecture & Planning, and Management backgrounds. It aims to promote multidisciplinary research and education for manpower development in transportation systems encompassing all modes of transport such as Road Transport, Rail Transport, Inland Water Transport, Air Transportation, Pipeline Transport, etc.",
   },
